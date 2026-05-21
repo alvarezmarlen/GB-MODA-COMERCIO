2cef3cd696b2cf7a5add2c61ab440fc914a217aa
alvarezmarlen <marlenjosefalvarez@gmail.com>
2026-05-21 12:25:06 +0200

    prueba

--- backend/app/run.py
+++
@@ -1,8 +0,0 @@
-from app.app import create_app
-
-# Llamamos a la función constructora que tiene toda la configuración del equipo
-app = create_app()
-
-if __name__ == "__main__":
-    # Arrancamos el servidor
-    app.run(host="0.0.0.0", port=5000, debug=True)
