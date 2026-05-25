83a9ba7a08247e415b027025642e25c3f37eef33
alvarezmarlen <marlenjosefalvarez@gmail.com>
2026-05-25 10:42:44 +0200

    tests languajes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -10,5 +11,9 @@ export default defineConfig({
       '/uploads': 'http://localhost:5000',
       '/users': 'http://localhost:5000',
     }
+  },
+  test: {
+    environment: 'happy-dom',
+    globals: true,
   }
 })
