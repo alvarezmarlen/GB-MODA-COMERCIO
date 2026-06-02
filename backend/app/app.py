2f800a80a25a3912fc0ef57a70a6743b1d4c89c9
Naia <naia.arenaza88@gmail.com>
2026-06-02 09:35:00 +0200

    docker y base de datos mejorados

--- backend/app/app.py
+++ backend/app/app.py
@@ -16,11 +16,6 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object('app.core.config.Config')
-    
-
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, '..', '..', 'mi_bd_sqlite', 'moda_comercio.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,9 +14,9 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, '..', '..', '..', 'mi_bd_sqlite', 'moda_comercio.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////app/mi_bd_sqlite/moda_comercio.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, '..', 'uploads', 'stories')
+    UPLOAD_FOLDER = '/app/mis_imagenes_locales/stories'
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
